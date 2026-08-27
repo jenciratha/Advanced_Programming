62b7c9242199ed79cdf13ada121dddc915fb075a
Jencika Rathakrishnan <124652698+jenciratha@users.noreply.github.com>
2026-08-27 22:11:17 +0530

    Create BusinessException.java
    Added BusinessException

---
+++ SunriseDentalClinicServlet/SunriseDentalClinicServlet/src/main/java/lk/edu/icbt/sunrise/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package lk.edu.icbt.sunrise.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
